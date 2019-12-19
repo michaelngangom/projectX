f7b33eab895c3a2991423385e27ec32b63e6a6e7
root <michael.ngangom@gmail.com>
2019-12-19 08:59:29 +0000

    initial commit

--- server.js
+++ server.js
@@ -8,6 +8,18 @@ let app = Express();
 var cookieParser = require('cookie-parser');
 var session      = require('express-session');
 
+var winston = require('winston');
+var {Loggly} = require('winston-loggly-bulk');
+
+winston.add(new Loggly({
+    token: "8497d33b-3a57-4698-9584-350c6dc9a9bd",
+    subdomain: "michaelngangom",
+    tags: ["Winston-NodeJS"],
+    json: true
+}));
+
+winston.log('allAccounts', "TestLog");
+
 var flash = require('req-flash');
 
 app.use(cookieParser());
@@ -21,7 +33,7 @@ app.use(BodyParser.urlencoded({
 app.use(BodyParser.json());
 
 
-let token1,token2,authHolds,authHoldFormat,auth;
+let token1,token2,authHolds,authHoldFormat,auth,allAccountsRaw;
 let resultAPI,resultAPITSend,allTransactions,allAccounts;
 
 auth=config.basicAuth;
@@ -32,6 +44,11 @@ app.set('view engine','ejs');
 
 var CONNECTION_URL='mongodb+srv://dbadmin:mongodb123@cluster0-grfcl.mongodb.net/test?retryWrites=true';
 const DATABASE_NAME = "CardToken";
+
+
+app.post("/createClient", (request,response)=>{
+  response.render('../views/pages/cleint');
+});
 
 app.get("/",(request, response)=>{
 	response.render("hello");
@@ -48,6 +65,7 @@ app.post("/createToken", (request, response) => {
         if(error) {
             return response.status(500).send(error);
         }
+		//response.status(204).send(result);
 		request.flash("The card token has been saved in the database");
         response.redirect('/index');
     });
@@ -60,11 +78,53 @@ app.post("/getToken", (request, response) => {
         response.render("../views/pages/cards",{result:result});
     });
 });
+
+
+app.post("/getAccountFromToken", (request, response) => {
+collection.find({tokenCode:request.body.tokenID}).toArray(function(error, documents) {
+    if (error) {}else{
+	
+		console.log(documents);
+		response.render("../views/pages/cards",{result:documents});
+    //res.send(documents);
+	}
+});
+		// 
+   
+});
+
+app.post("/delink", (request,response)=>{
+		delink(request.body.accountAHID,request.body.token);
+	response.redirect('/index');
+	
+});
+
+
+function delink(account,tokenpassed){
+	var options = { method: 'DELETE',
+				     url: 'https://michael.mambu.com/api/deposits/'+account+'/cards/'+tokenpassed,
+  headers: 
+   { 'cache-control': 'no-cache',
+     Connection: 'keep-alive',   
+    Authorization: auth,
+     Accept: 'application/vnd.mambu.v2+json',
+     'Content-Type': 'application/json' },
+
+  json: true };
+
+requests(options, function (error, response, body) {
+  if (error) throw new Error(error);
+
+  console.log(body);
+});
+}
+
 //method to get the tokens for an account
 function getCard(AID){
 	console.log('inside the function');
-	var endpoint= 'https://michael.sandbox.mambu.com/api/deposits/'+AID+'/cards';
+	var endpoint= 'https://michael.mambu.com/api/deposits/'+AID+'/cards';
 	console.log(endpoint);
+
 	var options = { method: 'GET',
   url: endpoint,
   headers:
@@ -90,7 +150,12 @@ requests(options, function (error, response, body) {
 //This is where we will get the request to pull the tokens based on an account ID
 app.post('/getCards', (request, response) => {
 	console.log(request.body.accountID);
-	getCard(request.body.accountID);
+	
+	
+		setTimeout(function(){
+	console.log('take some time!');
+	getCard(request.body.accountID);}, 4000);
+	
   response.render('../views/pages/payment', {token2:token2} );
 });
 app.post("/webhookmambu", (req, res, next) => {
@@ -104,6 +169,12 @@ app.post("/webhookmambu", (req, res, next) => {
   res.status(200).send("OK");
 });
 
+app.post("/apptest", (req, res, next) => {
+
+	res.render("../views/pages/index");
+
+
+});
 app.get("/getToken/:id", (request, response) => {
     collection.findOne({ "_id": new ObjectId(request.params.id) }, (error, result) => {
         if(error) {
@@ -132,6 +203,9 @@ app.post('/financialtransactions',(request,response) =>{
 		case "Complete":
 			complete(cardNumber,externalAuthorizationReferenceId,amount);
 			break;
+		case "Refund":
+			refund(cardNumber,externalAuthorizationReferenceId,amount);
+			break;
 		default:{
      	 complete(cardNumber,externalAuthorizationReferenceId,amount);
 		}
@@ -146,7 +220,7 @@ app.post('/authorizationholds',(request,response) =>{
 	var externalAuthorizationReferenceId =request.body.externalAuthorizationReferenceId;
 	var cardNumber = request.body.cardToken;
 	console.log("we are in auth hold - " + cardNumber);
-	var endpoint = 'https://michael.sandbox.mambu.com/api/cards/' + cardNumber + '/authorizationholds';
+	var endpoint = 'https://michael.mambu.com/api/cards/' + cardNumber + '/authorizationholds';
 	var amount =request.body.amount;
 	console.log(endpoint);
 
@@ -190,15 +264,23 @@ app.get('/index',(request,response)=>{
 	response.render('pages/index');
 });
 
+app.get('/getAllOptions',(request,response)=>{
+	response.render('pages/allmenu');
+});
+
+
+
 //To get all the transactions on an account.
 app.post('/getAllTransaction',(request,response)=>
 		{
 		getAllTransactions(request.body.accountAHID);
+	//response.render('pages/index');
+	
     //response.render('../views/pages/transactions', {allTransactions:allTransactions} );
 		setTimeout(function(){
 		console.log(allTransactions);
 		response.render('../views/pages/transactions', {allTransactions:allTransactions} );
-}, 4000);
+}, 8000);
 });
 //get all accounts
 app.post('/getAccount', function(request,response){
@@ -209,7 +291,7 @@ app.post('/getAccount', function(request,response){
 //Function to get all deposit accounts on the whole platform
 function getAccount(){
 var options = { method: 'GET',
-  url: 'https://michael.sandbox.mambu.com/api/deposits',
+  url: 'https://michael.mambu.com/api/deposits',
   headers:
    { 'cache-control': 'no-cache',
      Connection: 'keep-alive',
@@ -219,14 +301,20 @@ var options = { method: 'GET',
 };
 requests(options, function (error, response, body) {
   if (error) throw new Error(error);
-  console.log(response);
+ // console.log(response);
+	allAccountsRaw=body;
   allAccounts=JSON.parse(body);
+	
+	console.log(allAccounts);
+	//winston.log('allAccounts', allAccounts);
 });
 }
 //method to get all the auth hold transactions
 function getAllTransactions(Account){
-var endpoint = 'https://michael.sandbox.mambu.com/api/deposits/'+Account+'/transactions?detailsLevel=Full';
-console.log(endpoint);
+var endpoint = 'https://michael.mambu.com/api/deposits/'+Account+'/transactions?detailsLevel=Full';
+console.log(endpoint);	
+	
+
 
 var options = { method: 'GET',
   url: endpoint,
@@ -240,16 +328,21 @@ var options = { method: 'GET',
 
 requests(options, function (error, response, body) {
   if (error){
-	  console.log('error');
+	  console.log('error'+error);
   }
 	else{
+		//console.log(response);
+		console.log(body);
 		allTransactions = JSON.parse(body);
+		console.log(allTransactions);
+		winston.log('allTransactions', allTransactions);
+
 	}
 });
-}
+ }
 
 function getAuthHolds(Account){
-var endpoint = 'https://michael.sandbox.mambu.com/api/deposits/'+Account+'/authorizationholds';
+var endpoint = 'https://michael.mambu.com/api/deposits/'+Account+'/authorizationholds';
 console.log(endpoint);
 
 var options = { method: 'GET',
@@ -268,8 +361,9 @@ requests(options, function (error, response, body) {
 	else{
 		//console.log('else of the function');
 		token1 = JSON.parse(body);
-		authHoldFormat=body;
- 		console.log(token1 + "another same token " + authHoldFormat);
+		//authHoldFormat=body;
+		// console.log(token1 + "another same token " + authHoldFormat);
+		// winston.log('authHoldFormat', authHoldFormat);
 	}
 });
 }
@@ -288,6 +382,40 @@ app.post('/getAuthorizationholds', (request, response) => {
 }, 4000);
 });
 
+app.post("/getAuthHoldForToken", (request, resposne)=>{
+	
+	setTimeout(function(){
+	console.log('take some time!');
+	getAccount();}, 4000);
+	
+		setTimeout(function(){
+	 console.log('take some time!');
+	 console.log(allAccounts.length);}, 8000);
+	
+		setTimeout(function(){
+	console.log('take some time!');
+	test();}, 9000);
+	
+	
+	
+	// let allData=[];
+	// for (var i=0;i<allAccounts.length;i++){
+	// 	allData = allData + getAuthHolds(allAccounts[i].id);
+	// }
+	// console.log(allData);
+	
+	
+});
+
+function test()
+{
+	let allData=[];
+	for (var i=0;i<allAccounts.length;i++){
+		allData = allData + getAuthHolds(allAccounts[i].id);
+	}
+	console.log(allData);
+}
+
 app.post('/linkToken',(request,response)=>{
 	linkCard(request.body.accountAHID,request.body.token);
 	response.redirect('/index');
@@ -298,7 +426,7 @@ app.post('/linkToken',(request,response)=>{
 
 function linkCard(account,token){
 var options = { method: 'POST',
-  url: 'https://michael.sandbox.mambu.com/api/deposits/'+account+'/cards',
+  url: 'https://michael.mambu.com/api/deposits/'+account+'/cards',
   headers:
    { 'cache-control': 'no-cache',
      Connection: 'keep-alive',
@@ -319,7 +447,7 @@ requests(options, function (error, response, body) {
 
 function complete(cardnum,reference,amount){
 
-	let endpoint = 'https://michael.sandbox.mambu.com/api/cards/' + cardnum + '/financialtransactions';
+	let endpoint = 'https://michael.mambu.com/api/cards/' + cardnum + '/financialtransactions';
 	console.log('complete' + endpoint);
     var options = { method: 'POST',
   url: endpoint ,
@@ -360,7 +488,7 @@ requests(options, function (error, response, body) {
 
 function increase(cardnum,reference,amount){
 	console.log("increase");
-	let url='https://michael.sandbox.mambu.com/api/cards/'+cardnum+'/authorizationholds/'+reference+':increase';
+	let url='https://michael.mambu.com/api/cards/'+cardnum+'/authorizationholds/'+reference+':increase';
 	console.log(url);
 var options = { method: 'POST',
   url: url,
@@ -385,7 +513,7 @@ requests(options, function (error, response, body) {
 //2.Decrease
 
 function decrease(cardnum,reference,amount){
-	let url='https://michael.sandbox.mambu.com/api/cards/'+cardnum+'/authorizationholds/'+reference+':decrease';
+	let url='https://michael.mambu.com/api/cards/'+cardnum+'/authorizationholds/'+reference+':decrease';
 var options = { method: 'POST',
   url: url,
   headers:
@@ -408,7 +536,7 @@ requests(options, function (error, response, body) {
 
 // 3. Delete
 function deleteTransaction(cardnum,reference){
-	let url='https://michael.sandbox.mambu.com/api/cards/'+cardnum+'/authorizationholds/'+reference;
+	let url='https://michael.mambu.com/api/cards/'+cardnum+'/authorizationholds/'+reference;
 var options = { method: 'DELETE',
   url: url,
   headers:
@@ -424,14 +552,44 @@ requests(options, function (error, response, body) {
   console.log(body);
 });
 }
+
+// 4. Refund
+function refund(cardnum,reference,amount){
+	
+	
+let url='https://michael.mambu.com/api/cards/'+cardnum+'/financialtransactions/'+reference+":decrease";
+	console.log(url);
+var options = { method: 'POST',
+  url: url,
+  headers:
+   { 'cache-control': 'no-cache',
+     Connection: 'keep-alive',
+     Authorization: auth,
+     Accept: 'application/vnd.mambu.v2+json',
+     'Content-Type': 'application/json' },
+			   
+body: 
+   { externalReferenceId: reference,
+     amount: amount,
+     currencyCode: 'EUR',
+     transactionChannelId: 'cash' },
+  json: true };
+
+requests(options, function (error, response, body) {
+  if (error) throw new Error(error);
+  console.log(body);
+});
+}
+
+
 app.listen(3000, () => {
-	console.log('Server started at 3000');
+	console.log("connected to port 3000");
 	 MongoClient.connect(CONNECTION_URL, { useNewUrlParser: true }, (error, client) => {
         if(error) {
             throw error;
         }
         database = client.db(DATABASE_NAME);
-        collection = database.collection("token");
+        collection = database.collection("token"); 
 		 collectionNotification=database.collection("cardNotification");
         console.log("Connected to `" + DATABASE_NAME + "`!");
     });
